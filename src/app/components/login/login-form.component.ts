08e7f40cb565c10e02f545721e5a438fc505a4ba
reethfx <reethb3rsrk@gmail.com>
2024-11-18 13:38:30 +0100

    login base logic

--- src/app/components/login/login-form.component.ts
+++ src/app/components/login/login-form.component.ts
@@ -1,11 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: 'login-form.component.html',
   styleUrls: ['login-form.component.scss'],
   standalone: true,
-  imports: []
+  imports: [FormsModule, NgIf]
 })
 
-export class LoginFormComponent  {}
+export class LoginFormComponent  {
+
+  constructor(private router: Router) {}
+  
+  private validUser = {
+    username: 'proyectos@pil.es',
+    password: 'test1234'
+  };
+
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  onLogin() {
+    if (
+      this.username === this.validUser.username &&
+      this.password === this.validUser.password
+    ) {
+      console.log('Login exitoso');
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Usuario o contraseña incorrectos';
+    }
+  }
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,6 @@
+// src/app/models/user.ts
+export interface User {
+    username: string;
+    password: string;
+  }
+  
