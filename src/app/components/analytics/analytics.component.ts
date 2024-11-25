88f7eba9f242a24476c01b00701d906fb0df250b
reethfx <reethb3rsrk@gmail.com>
2024-11-25 17:24:49 +0100

    table inizialitation

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -1,15 +1,15 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BasicChartComponent } from '../charts/basic-chart/basic-chart.component';
 import { Router } from '@angular/router';
+
+import { BasicChartComponent } from '../charts/basic-chart/basic-chart.component';
+import { CallsChartComponent } from '../charts/calls-chart/calls-chart.component';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: 'analytics.component.html',
   styleUrls: ['analytics.component.scss'],
   standalone: true,
-  imports: [BasicChartComponent]
+  imports: [BasicChartComponent, CallsChartComponent]
 })
 
 export class AnalyiticsComponent  {
---
+++ src/app/components/charts/calls-chart/calls-chart.component.ts
@@ -0,0 +1,16 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-calls-chart',
+  templateUrl: 'calls-chart.component.html',
+  styleUrls: ['calls-chart.component.scss'],
+  standalone: true,
+  imports: [FormsModule],
+})
+
+export class CallsChartComponent  {
+
+}
