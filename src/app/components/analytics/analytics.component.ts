bd9d5137d703bd2afa59a16eb785a0c3ff309388
reethfx <reethb3rsrk@gmail.com>
2024-11-27 11:39:58 +0100

    graph update

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -3,13 +3,15 @@ import { Router } from '@angular/router';
 
 import { BasicChartComponent } from '../charts/basic-chart/basic-chart.component';
 import { CallsChartComponent } from '../charts/calls-chart/calls-chart.component';
+import { AnalyiticsCardsComponent } from '../cards/analytics-cards/anayltics-cards.component';
+
 
 @Component({
   selector: 'app-login-form',
   templateUrl: 'analytics.component.html',
   styleUrls: ['analytics.component.scss'],
   standalone: true,
-  imports: [BasicChartComponent, CallsChartComponent]
+  imports: [BasicChartComponent, CallsChartComponent, AnalyiticsCardsComponent],
 })
 
 export class AnalyiticsComponent  {
---
+++ src/app/components/cards/analytics-cards/anayltics-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-analytics-cards',
+  templateUrl: 'anayltics-cards.component.html',
+  styleUrls: ['anayltics-cards.component.scss'],
+  standalone: true,
+  imports: [FormsModule],
+})
+
+export class AnalyiticsCardsComponent  {
+
+}
