dd088673af95276774969b8c028f8d21262ca551
Mario Rodriguez <mrodriguez@oficina.local>
2024-12-16 17:44:01 +0100

    cambio de href a routerLink

--- src/app/components/charts/basic-chart/basic-chart.component.ts
+++ src/app/components/charts/basic-chart/basic-chart.component.ts
@@ -28,7 +28,7 @@ export class BasicChartComponent implements OnInit {
 
     //dependiendo del mes en el que estemos o sea poner dias
     this.chartDia = this.createChart('dia', 
-      ['00h', '01h', '02h', '03h', '05h','04h', '06h','07h','08h','09h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h','23h', '24h'], 
+      ['00h', '01h', '02h', '03h', '05h','04h', '06h','07h','08h','09h','10h','11h','12h','13h','14h','15h','16h','17h','18h','19h','20h','21h','22h','23h'], 
       [42, 38, 62, 50, 71, 60, 31, 80, 90, 91, 16, 74, 130, 0, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240]);
     
     this.chartMes = this.createChart('mes', 
---
+++ src/app/services/consumos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { develop, production } from "../../environments/environment";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class ConsumosServices {
+   
+    private apiUrl = develop.API_URL;
+    constructor(private http: HttpClient) { }
+
+    getAllCalls(): Observable<any[]> {
+      return this.http.get<any[]>(`${this.apiUrl}/consumos`);
+    }
+}
+
